90338cac589afa942bd0e47254eff7b4a573e628
木瓜丸 <boke0@eugensite.com>
2020-09-04 02:39:10 +0900

    [update]

--- mitama/portal/views.py
+++ mitama/portal/views.py
@@ -1,6 +1,6 @@
 from mitama.http import Response, get_session
 from mitama.app import Template, PackageLoader, Environment
-from mitama.auth import password_auth, AuthorizationError
+from mitama.auth import password_auth, get_jwt, AuthorizationError
 
 env = Environment(loader = PackageLoader(__package__, './templates'))
 
@@ -11,20 +11,20 @@ async def home(request):
 async def login(request):
     data = {}
     if request.method == 'POST':
-        post = request.post()
-        screen_name = post['screen_name'] or ''
-        password = post['password'] or ''
+        post = await request.post()
+        screen_name = post.get('screen_name', '')
+        password = post.get('password', '')
         data['screen_name'] = screen_name
         data['password'] = password
         try:
             result = password_auth(screen_name, password)
-            sess = get_session(request)
-            sess['jwt_token'] = result.getJWT()
-            redirect_to = request.query['redirect_to'] or ''
+            sess = await get_session(request)
+            sess['jwt_token'] = get_jwt(result).decode()
+            redirect_to = request.query.get('redirect_to', '')
             return Response(
-                headers=[
-                    'Location: '+redirect_to
-                ]
+                headers={
+                    'Location': redirect_to
+                }
             )
         except AuthorizationError as err:
             data['error'] = 'パスワード、またはログイン名が間違っています'
