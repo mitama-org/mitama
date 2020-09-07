85f17746ed3c3d172ae732e2b94a5082df45c4c5
木瓜丸 <boke0@eugensite.com>
2020-09-07 21:24:49 +0900

    [add] ログイン状態の管理ミドルウェアの追加

--- mitama/portal/main.py
+++ mitama/portal/main.py
@@ -1,28 +1,30 @@
 from . import Metadata, urls
 from mitama.app import App
-from mitama.http import middleware, get_session
-from mitama.auth import check_jwt, AuthorizationError
+from mitama.http import middleware, get_session, Response
+from mitama.http.auth import get_login_state
+from mitama.auth import AuthorizationError
+from mitama.nodes import User
 import urllib
 
 meta = Metadata()
-app = App(meta)
 
 @middleware
 async def initialize_middleware(request, handler):
     if User.query.count() == 0:
-        from .views import setup
-        return await setup(request)
+        return Response(headers = {
+            'Location': app.convert_uri('/setup')
+        })
     else:
         return await handler(request)
 
 @middleware
 async def session_middleware(request, handler):
     try:
-        user = get_login_state(request)
+        await get_login_state(request)
     except AuthorizationError:
-        return Response(headers = {
-            'Location': app.convert_uri('/login?'+urllib.parse.quote(request.uri))
-        })
+        if app.revert_uri(request.url).path != '/login':
+            return Response.redirect(app.convert_uri('/login?'+urllib.parse.quote(str(request.url))), status = 301)
     return await handler(request)
 
+app = App(meta, middlewares = [initialize_middleware, session_middleware])
 app.router.add_routes(urls.urls)
--- mitama/portal/urls.py
+++ mitama/portal/urls.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 from mitama.http.method import *
 from . import views
-from views import user
-from views import group
+from .views import user
+from .views import group
 
 project_dir = Path(os.path.dirname(__file__))
 
@@ -13,7 +13,7 @@ urls = [
     view('/login', views.login),
     view('/signup', views.signup),
     view('/users', user.list),
-    view('/users/invite', user.invite),
+    view('/users/invite', user.create),
     view('/users/{id}', user.retrieve),
     view('/users/{id}/settings', user.update),
     view('/users/{id}/delete', user.delete),
