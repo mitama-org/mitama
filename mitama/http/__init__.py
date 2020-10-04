6c47c3b74b1ada2210b9f1a49def03b7381a630f
木瓜丸 <boke0@eugensite.com>
2020-10-04 23:06:35 +0900

    [update]

--- mitama/http/__init__.py
+++ mitama/http/__init__.py
@@ -4,6 +4,7 @@ from http.server import ThreadingHTTPServer
 from socketserver import StreamRequestHandler
 from .request import Request
 from .response import Response
+import threading
 
 
 def run_app(app, port, request_factory = Request.parse_stream, ssl = False):
@@ -12,10 +13,26 @@ def run_app(app, port, request_factory = Request.parse_stream, ssl = False):
             request = request_factory(self.rfile)
             response = app(request)
             response.start(request, self.wfile)
-    with ThreadingHTTPServer(('', int(port)), RequestHandler) as server:
-        if ssl!=False:
-            ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-            crtx.load_cert_chain(ssl['cert'], ssl['key'])
-            ctx.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
-            server.socket = ctx.wrap_socket(server.socket)
-        server.serve_forever()
+    class SSLRequestHandler(StreamRequestHandler):
+        def handle(self):
+            request = request_factory(self.rfile, True)
+            response = app(request)
+            response.start(request, self.wfile)
+    def serve():
+        with ThreadingHTTPServer(('', int(port)), RequestHandler) as server:
+            server.serve_forever()
+    th_nossl = threading.Thread(name = 'nossl', target = serve)
+    if ssl != False:
+        def ssl_serve():
+            with ThreadingHTTPServer(('', int(ssl['port'])), SSLRequestHandler) as server:
+                if ssl!=False:
+                    ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+                    crtx.load_cert_chain(ssl['cert'], ssl['key'])
+                    ctx.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+                    server.socket = ctx.wrap_socket(server.socket)
+                server.serve_forever()
+        th_ssl = threading.Thread(name = 'ssl', target = ssl_serve)
+        th_ssl.start()
+        th_nossl.start()
+    else:
+        th_nossl.start()
