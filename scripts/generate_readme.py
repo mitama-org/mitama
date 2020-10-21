f574787a4f457fbad48ff098f4d07c2ed71c38a2
木瓜丸 <boke0@eugensite.com>
2020-10-22 00:57:38 +0900

    add

---
+++ scripts/generate_readme.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from jinja2 import Environment, FileSystemLoader
+import glob
+import os
+import json
+from pathlib import Path
+
+CWD = Path(os.path.dirname(__file__))
+
+env = Environment(loader = FileSystemLoader(CWD / 'files/templates'))
+tmp = env.get_template('README.jinja.md')
+
+langs = dict()
+for filename in glob.glob(str(CWD / 'files/readme/*.json')):
+    lang = os.path.splitext(os.path.basename(filename))[0]
+    with open(filename) as f:
+        langs[lang] = json.loads(f.read())
+
+print(langs)
+for lang in langs.keys():
+    with open(CWD / ('../README.'+lang+'.md'), 'w') as f:
+        f.write(tmp.render(langs[lang]))
