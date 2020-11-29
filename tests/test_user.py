a5611f59e258610ab522f827fa5ef4e902d89658
SeisukeIto <esaku0207@gmail.com>
2020-11-30 00:58:36 +0900

    add password validation

--- tests/test_user.py
+++ tests/test_user.py
@@ -12,8 +12,8 @@ class TestUser(unittest.TestCase):
         user = User()
         user.name = "alice"
         user.screen_name = "alice"
-        user.set_password("alice_s_password")
+        user.set_password("alice_s_password_1234")
         user.create()
-        self.assertTrue(User.password_auth("alice", "alice_s_password"))
+        self.assertTrue(User.password_auth("alice", "alice_s_password_1234"))
         jwt = user.get_jwt()
         self.assertEqual(User.check_jwt(jwt), user)
