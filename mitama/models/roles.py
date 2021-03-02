99e33ad5b46288599b81ac0e607e0214104f18ea
boke0 <speken00.tt@gmail.com>
2021-03-02 21:57:25 +0900

    [fix]

--- mitama/models/roles.py
+++ mitama/models/roles.py
@@ -1,10 +1,11 @@
 from mitama.db import BaseDatabase, func, ForeignKey, relationship, Table, backref
-from mitama.db.types import Column, Group, Integer, LargeBinary
+from mitama.db.types import Column, Integer, LargeBinary
 from mitama.db.types import Node as NodeType
 from mitama.db.types import String
 from mitama.db.model import UUID
 
 from .core_db import db
+from .nodes import User, Group, UserGroup
 
 role_user = Table(
     "mitama_role_user",
